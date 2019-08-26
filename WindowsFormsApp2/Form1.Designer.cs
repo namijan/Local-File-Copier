bf820f4e495f5dcbf6495c3f32c014f5e1e3ca3b
namijan <namijan@gmail.com>
2019-08-26 13:02:23 +0530

    Test Version

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(4, 5);
+            this.treeView1.Margin = new System.Windows.Forms.Padding(1);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(412, 334);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(459, 306);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "In testing stage. Not to be distributed or sold";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(455, 323);
+            this.label2.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "For any information, contact ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(759, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 50);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Backup";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(435, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(244, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(435, 57);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(294, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Include unrecognized files (with model GUID as filename)";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "No project selected"});
+            this.listBox1.Location = new System.Drawing.Point(435, 99);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(318, 199);
+            this.listBox1.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(685, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(432, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Select Folder:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(432, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Select Files:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(589, 322);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(142, 13);
+            this.linkLabel1.TabIndex = 13;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Tag = "";
+            this.linkLabel1.Text = "namit.ranjan@autodesk.com";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 345);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(1);
+            this.MaximumSize = new System.Drawing.Size(856, 384);
+            this.MinimumSize = new System.Drawing.Size(856, 384);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Tag = "mailto:youremail@xx.com";
+            this.Text = "Local File Copier (Beta)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        protected System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            try
+            {
+                InitializeComponent();
+            textBox1.Text= (System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments))+"\\Local Files";
+            string[] folders = System.IO.Directory.GetDirectories(@Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "/Autodesk/Revit", "*", System.IO.SearchOption.TopDirectoryOnly);
+            string[] words = new string[] { };
+            
+                foreach (String single in folders)  //building the parent
+                {
+                    if (single.Contains("Autodesk Revit"))
+                    {
+                        words = single.Split(' ');
+                        string version = words[words.Length - 1];
+                        treeView1.Nodes.Add(version, "Revit " + version);
+                        string[] basefolders = System.IO.Directory.GetDirectories(@Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "/Autodesk/Revit/Autodesk Revit " + version + "/CollaborationCache", "*", System.IO.SearchOption.TopDirectoryOnly);
+
+                        foreach (String Single in basefolders)
+                        {
+                            words = Single.Split('\\');
+                            string accountGUID = words[words.Length - 1];
+                            treeView1.Nodes[version].Nodes.Add(accountGUID, accountGUID);
+
+                            string[] projfolders = System.IO.Directory.GetDirectories(@Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "/Autodesk/Revit/Autodesk Revit " + version + "/CollaborationCache/" + accountGUID, "*", System.IO.SearchOption.TopDirectoryOnly);
+                            foreach (String eachproj in projfolders)
+                            {
+                                words = eachproj.Split('\\');
+                                string ProjectGUID = words[words.Length - 1];
+
+                                //look for projectGUID in journal
+
+                                string[] journals = System.IO.Directory.GetDirectories(@Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "/Autodesk/Revit/Autodesk Revit " + version + "/Journals", "*", System.IO.SearchOption.TopDirectoryOnly);
+                                string arr = "";
+                                string contents = "";
+                                foreach (string file in Directory.EnumerateFiles(@Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "/Autodesk/Revit/Autodesk Revit " + version + "/Journals", "*.txt"))
+                                {
+
+                                    FileInfo filein = new FileInfo(file);
+                                    if (IsFileLocked(filein) == false)
+                                        contents = File.ReadAllText(file);
+
+                                    if (contents.Contains(ProjectGUID))
+                                    {
+                                        arr = contents.Substring((contents.IndexOf(ProjectGUID) + ProjectGUID.Length), 150);
+                                        if (arr[0].Equals('}'))
+                                        {
+
+
+
+                                            String projname = (arr.Split('\\'))[0];
+                                            projname = projname.Split('"')[0];
+                                            projname = projname.Substring(1);
+                                            projname = projname.Replace("%20", " ");
+
+                                            //
+                                            TreeNode final = new TreeNode(projname + " (" + ProjectGUID + ")");
+                                            if (!(treeView1.Nodes[version].Nodes[accountGUID].Nodes.Contains(final)))
+                                            { treeView1.Nodes[version].Nodes[accountGUID].Nodes.Add(final); break; }
+
+
+
+                                        }
+                                    }
+
+                                }
+
+                                //  treeView1.Nodes[version].Nodes[accountGUID].Nodes.Add(ProjectGUID);
+
+
+
+                            }
+
+
+
+
+
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+               MessageBox.Show(ex.Message, "Error");
+                this.Close();
+            }
+
+
+
+    } 
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Length>1)
+            { 
+            try {
+                if (treeView1.SelectedNode.Level == 2)
+                {
+                    string projectGUID = treeView1.SelectedNode.Text.Split('(', ')')[1];
+
+                    String AccountID = treeView1.SelectedNode.Parent.Text;
+                    String version = treeView1.SelectedNode.Parent.Parent.Text;
+
+
+                    String DestinationPath = textBox1.Text + "\\" + treeView1.SelectedNode.Text + "\\" + DateTime.Now.ToShortDateString().Replace('/', '.') + DateTime.Now.ToLongTimeString().Replace(':', '.');
+                    String SourcePath = @Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Autodesk\\Revit\\Autodesk " + version + "\\CollaborationCache\\" + AccountID + "\\" + projectGUID;
+
+                    // MessageBox.Show(DestinationPath);
+                    foreach (string dirPath in Directory.GetDirectories(SourcePath, "*", SearchOption.AllDirectories))
+                        Directory.CreateDirectory(dirPath.Replace(SourcePath, DestinationPath));
+                    string finalpath = "";
+
+
+                    foreach (string newPath in Directory.GetFiles(SourcePath, "*.*", SearchOption.AllDirectories))
+                    {
+                        Cursor.Current = Cursors.WaitCursor;
+
+                        string[] words = newPath.Split('\\');
+                        string filename = words[words.Length - 1];
+                        if (filename.Contains("rvt"))
+                        {
+
+                            string receive = (parser(version, filename));
+
+
+                            if (receive.Length > 1)
+                            {
+                                finalpath = newPath.Replace(SourcePath, DestinationPath);
+
+                                foreach (string files in listBox1.SelectedItems)
+                                {
+                                    if (files == receive)                                   
+                                            File.Copy(newPath, finalpath.Replace(filename, receive), true);
+                                        
+                                }
+
+
+                            }
+                            else
+                            {
+                                if (checkBox1.Checked == true)
+                                { foreach (string files in listBox1.SelectedItems)
+                                    {
+                                        if (files == filename)
+                                                File.Copy(newPath, newPath.Replace(SourcePath, DestinationPath), true);
+
+                                    }
+                                }
+                            }
+
+
+
+
+
+                        }
+
+
+
+
+
+                    }
+
+                    processDirectory(DestinationPath);
+
+                    MessageBox.Show("Backup of " + treeView1.SelectedNode.Text + " created on " + DateTime.Now.ToShortDateString() + " at " + DateTime.Now.ToLongTimeString());
+                    Cursor.Current = Cursors.Default;
+                }
+           
+        
+            else
+                MessageBox.Show("Please select a project", "Error");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message+" Please attempt to copy manually.", "Error");
+            }
+            }
+            else
+                MessageBox.Show("Please select a folder", "Error");
+
+        }
+
+        private String parser(String version, String filename)
+        {
+            String modelGUID = filename.Split('.')[0];
+            string modelname="";
+            string arr = "";
+            foreach (string file in Directory.EnumerateFiles(@Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "/Autodesk/Revit/Autodesk " + version + "/Journals", "*.txt"))
+            {
+                string contents = "";
+
+                FileInfo filein = new FileInfo(file);
+                if (IsFileLocked(filein) == false)
+                {
+                    if ((file.Split('/')[file.Split('/').Length - 1]).Contains("journal"))
+                        contents = File.ReadAllText(file);
+                }    
+                if (contents.Contains('{'+modelGUID+'}'))
+                {
+                    arr = contents.Substring((contents.IndexOf('{' + modelGUID + '}') + ('{' + modelGUID + '}').Length), 150);
+
+                      int index = arr.IndexOf('"');
+                      if (index > 0)
+                         modelname = (arr.Substring(0, index)).Replace("%20"," ");
+                   
+                }
+
+                
+
+            }
+            if (modelname == "")
+                return ("");
+            else
+            return (modelname);
+        }
+
+        private static void processDirectory(string startLocation)
+        {
+            foreach (var directory in Directory.GetDirectories(startLocation))
+            {
+                processDirectory(directory);
+                if (Directory.GetFiles(directory).Length == 0 &&
+                    Directory.GetDirectories(directory).Length == 0)
+                {
+                    Directory.Delete(directory, false);
+                }
+            }
+        }
+
+        private void treeView1_AfterSelect_1(object sender, TreeViewEventArgs e)
+        {
+            listBox1.Items.Clear();
+            if (e.Node.Level == 2)
+            {
+                string projectGUID = e.Node.Text.Split('(', ')')[1];
+
+                String AccountID = e.Node.Parent.Text;
+                String version = e.Node.Parent.Parent.Text;
+                String SourcePath = @Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Autodesk\\Revit\\Autodesk " + version + "\\CollaborationCache\\" + AccountID + "\\" + projectGUID;
+
+        
+                foreach (string newPath in Directory.GetFiles(SourcePath, "*.*", SearchOption.AllDirectories))
+                {
+                    
+                    Cursor.Current = Cursors.WaitCursor;
+
+                    string[] words = newPath.Split('\\');
+                    string filename = words[words.Length - 1];
+                    if (filename.Contains("rvt"))
+                    {
+
+                        string receive = (parser(version, filename));
+
+
+                        if (receive.Length > 1)
+                        {
+                            if (!listBox1.Items.Contains(receive))
+                                listBox1.Items.Add(receive);
+                            
+                            
+
+                        }
+                        else
+                        {
+                            if (checkBox1.Checked == true)
+                            {
+                                if (!listBox1.Items.Contains(filename)) 
+                                    listBox1.Items.Add(filename);
+                                
+                            }
+
+                            }
+
+
+
+
+
+                    }
+
+
+
+
+
+                }
+
+               
+                Cursor.Current = Cursors.Default;
+            }
+            else
+                listBox1.Items.Add("No project selected");
+
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                listBox1.SetSelected(i, true);
+            }
+            listBox1.Sorted = true;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            treeView1.SelectedNode = null;
+            listBox1.Items.Add("No project selected");
+        }
+
+        protected virtual bool IsFileLocked(FileInfo file)
+        {
+            FileStream stream = null;
+
+            try
+            {
+                stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None);
+            }
+            catch (IOException)
+            {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return true;
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+
+            //file is not locked
+            return false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            textBox1.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabel1.LinkVisited = true;
+            System.Diagnostics.Process.Start("mailto:namit.ranjan@autodesk.com?subject=Local%20File%20Copier(Beta)");
+        }
+    }
+}
